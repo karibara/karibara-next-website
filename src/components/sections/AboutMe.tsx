ff3756091ed86f33c1c498233f7ef6c5dcb1e6da
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2025-02-10 16:39:08 +0100

    Implemented conditional logic for 'Read More' button to toggle content visibility.

--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -3,8 +3,12 @@
 import Image from "next/image";
 import { Button } from "../ui/button";
 import { GlassCard } from "../ui/GlassCard";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 export const AboutMe: React.FC = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <GlassCard className="grid lg:grid-cols-3 gap-2 max-w-3xl">
       <div className="w-28 lg:w-full">
@@ -26,29 +30,47 @@ export const AboutMe: React.FC = () => {
             logic, aesthetics, and good structure (not just in code!) are what
             really matter.
           </p>
-          <p className="py-6">
-            My components are so well thought out they could have their own
-            résumés. If a problem pops up, I’ll find a solution faster than
-            Ctrl+F. And when I don’t know something, I’m not afraid to ask —
-            good communication is key.
-          </p>
-          <p>
-            Away from the screen? You’ll probably find me sketching,
-            woodworking, running miles, drinking tea, or indulging in way too
-            much pasta.
-          </p>
-          <p className="py-6">
-            Always ready for new challenges—whether in code or creative
-            projects!
-          </p>
+          {!expanded && (
+            <Button onClick={() => setExpanded(!expanded)} className="mt-3">
+              more about me
+            </Button>
+          )}
+
+          {/* EXPANDED */}
+          <AnimatePresence>
+            {expanded && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.4, ease: "easeInOut" }}
+                className="overfolw-hidden"
+              >
+                <>
+                  <p className="py-6">
+                    My components are so well thought out they could have their
+                    own résumés. If a problem pops up, I’ll find a solution
+                    faster than Ctrl+F. And when I don’t know something, I’m not
+                    afraid to ask — good communication is key.
+                  </p>
+                  <p>
+                    Away from the screen? You’ll probably find me sketching,
+                    woodworking, running miles, drinking tea, or indulging in
+                    way too much pasta.
+                  </p>
+                  <p className="py-6">
+                    Always ready for new challenges—whether in code or creative
+                    projects!
+                  </p>
+                  <Button onClick={() => setExpanded(false)}>show less</Button>
+                </>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-        <Button>My linkedIn</Button>
       </div>
     </GlassCard>
   );
 };
-
-// I code, design, and sometimes even translate computers into human language. I specialize in frontend development, with React as my daily companion. I’m still new to the world of programming, but I catch on quickly—and my background in design has taught me that logic, aesthetics, and good structure (not just in code!) are what really matter.
-// My components are so well thought out they could have their own résumés. If a problem pops up, I’ll find a solution faster than Ctrl+F. And when I don’t know something, I’m not afraid to ask—good communication is key.
-// Away from the screen? You’ll probably find me sketching, woodworking, running miles, drinking tea, or indulging in way too much pasta.
-// Always ready for new challenges—whether in code or creative projects!
