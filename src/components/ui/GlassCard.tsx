141ee3c4c9ed7f1ac7544bb02133dc28e9637ac5
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2025-02-10 16:00:36 +0100

    Create GlassCard component

---
+++ src/components/ui/GlassCard.tsx
@@ -0,0 +1,16 @@
+type GlassCardProps = {
+  className?: string;
+};
+
+export const GlassCard: React.FC<React.PropsWithChildren<GlassCardProps>> = ({
+  children,
+  className,
+}) => {
+  return (
+    <div
+      className={`flex flex-wrap p-10 border border-peachKari rounded-3xl shadow-[0_10px_30px_rgba(212,181,162,0.6)] bg-gradient-to-br from-pink-300/10 via-purple-400/10 to-neutral-300/10 backdrop-blur-md ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
