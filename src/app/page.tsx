9851c7e3cf09669d8091c7c7d1337f48ac43b1c2
Malgorzata Bozykowska <malgorzata.bozykowska@gmail.com>
2025-10-27 09:16:08 +0100

    Refactor AboutMe section: reintroduce motion effects, add scroll-to-contact functionality, and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,28 +11,28 @@ export default function Home() {
       <div className="flex flex-col md:gap-16 items-center">
         <div className="relative w-full max-w-[654px] grid place-items-center gap-10 overflow-x-hidden md:overflow-visible">
           {/* HASH PATTERN */}
-          <Image
+
+          {/* <Image
             src="/hash-pattern.svg"
             alt="hash pattern"
             width={360}
             height={360}
             className="absolute top-20 -right-[40%] -z-10 "
-          />
+          /> */}
 
           {/* WAVE PATTERN */}
-          <Image
+          {/* <Image
             src="/waves.svg"
             alt="waves pattern"
             width={156}
             height={58}
             className="absolute top-[10rem] lg:top-[13rem] scale-75 lg:scale-100 -left-14 -z-10 "
-          />
+          /> */}
 
           {/* WELCOME - SECTION */}
           <WelcomeSection />
 
           {/* ABOUT ME - SECTION*/}
-
           <AboutMe />
         </div>
         {/* SKILLS - SECTION*/}
--- src/components/sections/AboutMe.tsx
+++ src/components/sections/AboutMe.tsx
@@ -2,47 +2,99 @@
 
 import Image from "next/image";
 import { Button } from "../ui/button";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 export const AboutMe: React.FC = () => {
+  // scroll to contact section
+  const handleScrollToContact = () => {
+    const section = document.getElementById("contact");
+    section?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    // <motion.div
-    //   initial={{ scale: 1, opacity: 1, filter: "blur(15px)" }}
-    //   whileInView={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
-    //   transition={{
-    //     type: "spring",
-    //     stiffness: 80,
-    //     damping: 60,
-    //     duration: 8,
-    //     delay: 4,
-    //   }}
-    // >
-    <div className="relative max-w-[654px] mx-auto ">
-      <div className=" grid grid-cols-2 items-center relative">
-        {/* IMAGE - mobile */}
-        <div className="order-1 md:order-2 relative flex  justify-end overflow-visible">
-          <div className="relative w-64 h-72 md:w-[380px] md:h-[420px] overflow-visible">
-            <Image
-              src="/profile-photo-no-bg.png"
-              alt="profile photo"
-              fill
-              className="object-contain -scale-x-[1.25] scale-y-[1.25] md:-scale-x-[1.5] md:scale-y-[1.5] -translate-y-10 lg:translate-x-16    z-10 pointer-events-none select-none"
-            />
+    <div className="relative max-w-[654px] mx-auto">
+      {/* HASH PATTERN */}
+      <motion.div
+        initial={{ scale: 1.5, opacity: 0, filter: "blur(15px)" }}
+        animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
+        transition={{
+          type: "spring",
+          stiffness: 80,
+          damping: 60,
+          duration: 10,
+          delay: 3,
+        }}
+      >
+        <Image
+          src="/hash-pattern.svg"
+          alt="hash pattern"
+          width={360}
+          height={360}
+          className="absolute -top-20 -right-[35%] -z-10 overflow-hidden"
+        />
+      </motion.div>
+
+      {/* WAVE PATTERN */}
+      <motion.div
+        initial={{ scale: 1.5, opacity: 0, filter: "blur(15px)" }}
+        animate={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
+        transition={{
+          type: "spring",
+          stiffness: 80,
+          damping: 60,
+          duration: 10,
+          delay: 2,
+        }}
+      >
+        <Image
+          src="/waves.svg"
+          alt="waves pattern"
+          width={156}
+          height={58}
+          className="absolute -top-12 lg:top-0 scale-75 lg:scale-100 -left-14 -z-10 "
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{ scale: 1, opacity: 0, filter: "blur(15px)" }}
+        whileInView={{ scale: 1, opacity: 1, filter: "blur(0px)" }}
+        transition={{
+          type: "spring",
+          stiffness: 80,
+          damping: 60,
+          duration: 8,
+          delay: 5,
+        }}
+      >
+        <div className=" grid grid-cols-2 items-center relative">
+          {/* IMAGE - mobile */}
+          <div className="order-1 md:order-2 relative flex  justify-end overflow-visible">
+            <div className="relative w-64 h-72 md:w-[380px] md:h-[420px] overflow-visible">
+              <Image
+                src="/profile-photo-no-bg.png"
+                alt="profile photo"
+                fill
+                className="object-contain -scale-x-[1.25] scale-y-[1.25] md:-scale-x-[1.5] md:scale-y-[1.5] -translate-y-10 lg:translate-x-16    z-10 pointer-events-none select-none"
+              />
+            </div>
+          </div>
+
+          {/* TEXT */}
+          <div className="order-2 md:order-1 relative z-20  p-6 md:pl-10  md:-mr-10">
+            <h3 className="text-lg lg:text-xl font-bold mb-3">About me</h3>
+            <p className="text-sm lg:text-base">
+              I code, design, and sometimes even translate computers into human
+              language. I specialize in frontend development, with React as my
+              daily companion.
+            </p>
+            <Button
+              onClick={handleScrollToContact}
+              className="mt-6 w-full md:w-auto"
+            >
+              contact me
+            </Button>
           </div>
         </div>
-
-        {/* TEXT */}
-        <div className="order-2 md:order-1 relative z-20  p-6 md:pl-10  md:-mr-10">
-          <h3 className="text-lg lg:text-xl font-bold mb-3">About me</h3>
-          <p className="text-sm lg:text-base">
-            I code, design, and sometimes even translate computers into human
-            language. I specialize in frontend development, with React as my
-            daily companion.
-          </p>
-          <Button className="mt-6 w-full md:w-auto">contact me</Button>
-        </div>
-      </div>
+      </motion.div>
     </div>
-    // </motion.div>
   );
 };
